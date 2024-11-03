415eda17c402b3a80a4ab46c7de790b6619319e6
ramosfabiano <ramos_fabiano@yahoo.com.br>
2024-11-02 22:35:46 -0300

    Formatting.

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -5,7 +5,7 @@ update_system() {
 }
 
 install_rpmfusion() {
-	dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+    dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
     dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     dnf -y update
 }
