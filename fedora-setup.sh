e0a8548a98ee85b638b72727dede0453e39fab1c
ramosfabiano <ramos_fabiano@yahoo.com.br>
2024-12-15 19:43:48 -0300

    Formatting.

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -79,7 +79,14 @@ install_veracrypt() {
 
 install_vscode() {
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    echo '
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+' > /etc/yum.repos.d/vscode.repo
     dnf -y install code
 }
 
