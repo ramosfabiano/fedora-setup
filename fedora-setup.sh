e007be44c93f8d844ed97550738b7c69bb6f92d3
ramosfabiano <ramos_fabiano@yahoo.com.br>
2025-02-02 19:35:06 -0300

    Remove firefox

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -62,6 +62,10 @@ setup_firewall() {
     firewall-cmd --list-all
 }
 
+remove_firefox() {
+    sudo dnf -y remove firefox
+}
+
 install_chrome() {
     dnf -y install google-chrome-stable
 }
@@ -193,6 +197,8 @@ auto() {
     setup_firewall
     msg 'Install MS fonts'
     setup_fonts
+    msg 'Remove firefox'
+    remove_firefox
     msg 'Install chrome'
     install_chrome
     msg 'Install veracrypt'
